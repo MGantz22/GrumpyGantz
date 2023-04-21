59945c47e30fa81d3703861e14bb00f4389f31df
Mitchell Gantz <mgantz22@icloud.com>
2023-04-21 14:53:17 -0700

    Add and update bean list

---
+++ src/components/BeanList.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Bean from "./Bean";
+import PropTypes from "prop-types";
+
+
+
+
+function BeanList(props){
+  return (
+    
+      
+    <React.Fragment>
+      <hr/>
+        {props.beanList.map((bean) => 
+          <Bean 
+            whenBeanClicked = {props.onBeanSelection}
+            name={bean.name}
+            origin={bean.origin}
+            roast={bean.roast}
+            price={bean.price}
+            stock={bean.stock}
+            sold={bean.sold}
+            id={bean.id}
+            key={bean.id} />
+        )}
+    </React.Fragment>
+    
+    
+  );
+}
+
+
+BeanList.propTypes = {
+  beanList: PropTypes.array,
+  onBeanSelection: PropTypes.func,
+}
+
+export default BeanList;
