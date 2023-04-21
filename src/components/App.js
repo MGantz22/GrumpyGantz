83238055d56ea1dc51cd93ee78643e4135399996
Mitchell Gantz <mgantz22@icloud.com>
2023-04-21 10:47:35 -0700

    Add and update app

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from './Header';
+// import BeanControl from './BeanControl';
+
+import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return(
+    <React.Fragment>
+      <Container>
+        <Header />
+        {/* <BeanControl /> */}
+      </Container>
+    </React.Fragment>
+  );
+}
+
+export default App;
