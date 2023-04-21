3968be60206e874eeb4d547f8187c21726c3df08
Mitchell Gantz <mgantz22@icloud.com>
2023-04-21 14:53:04 -0700

    Update title and spacing between buttons

--- src/components/BeanDetail.js
+++ src/components/BeanDetail.js
@@ -1,23 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+
 function BeanDetail(props){
   const { bean, onClickingDelete, onClickingEdit, onBeanSale } = props; 
 
   return (
     <React.Fragment>
-      <h1>GrumpyGantz Bean Detail</h1>
-      <ul>
-        <h3>{bean.name}</h3>
-        <li>Origin | {bean.origin}</li>
-        <li>Roast | {bean.roast}</li>
-        <li>Price | {bean.price}</li>
-        <li>In Stock | {bean.stock}</li>
-        <li>Stock Sold | {bean.sold}</li>
-      </ul>
-      <button onClick={onBeanSale}>Sell a Pound</button> 
-      <button onClick={onClickingEdit }>Edit Bean</button> 
-      <button onClick={()=> onClickingDelete(bean.id) }>Delete Bean</button> 
+      <div>
+        <h1>Bean Detail:</h1>
+        <ul>
+          <h3>{bean.name}</h3>
+          <li>Origin | {bean.origin}</li>
+          <li>Roast | {bean.roast}</li>
+          <li>Price | ${bean.price}</li>
+          <li>In Stock | {bean.stock}</li>
+          <li>Stock Sold | {bean.sold}</li>
+        </ul>
+      </div>
+
+      <button className="btn btn-success" onClick={(onBeanSale)}>Sell a Pound</button> &nbsp;
+      <button className="btn btn-warning" onClick={ onClickingEdit }>Edit Bean</button> &nbsp;
+      <button className="btn btn-danger" onClick={()=> onClickingDelete(bean.id) }>Delete Bean</button> 
       <hr/>
     </React.Fragment>
   );
