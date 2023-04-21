b5b06e40bd8abc9664da371241b3119753648649
Mitchell Gantz <mgantz22@icloud.com>
2023-04-21 11:59:30 -0700

    update App

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Header from './Header';
-// import BeanControl from './BeanControl';
+import BeanControl from './BeanControl';
 
 import Container from 'react-bootstrap/Container';
 // import Row from 'react-bootstrap/Row';
-// import Col from 'react-bootstrap/Col';
+import Col from 'react-bootstrap/Col';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
     <React.Fragment>
       <Container>
         <Header />
-        {/* <BeanControl /> */}
+       <Col><BeanControl /></Col>
       </Container>
     </React.Fragment>
   );
