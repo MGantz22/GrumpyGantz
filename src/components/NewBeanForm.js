21a1d51882b6a5c6dc6f44e20359c673b09ecb0f
Mitchell Gantz <mgantz22@icloud.com>
2023-04-21 16:34:51 -0700

    Add new Bean Form and update

---
+++ src/components/NewBeanForm.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { v4 } from 'uuid';
+import ReusableForm from './ReusableForm';
+import Container from 'react-bootstrap/Container';
+
+function NewBeanForm(props) {
+  return (
+    <React.Fragment>
+      <Container>
+      <ReusableForm 
+        formSubmissionHandler={handleNewBeanFormSubmission}
+        buttonText="Add bean Sack"  />
+      </Container>
+    </React.Fragment>
+  );
+
+  function handleNewBeanFormSubmission(event) {
+    event.preventDefault();
+    props.onNewBeanCreation({
+      name: event.target.name.value,
+      origin: event.target.origin.value,
+      price: parseFloat(event.target.price.value),
+      roast: event.target.roast.value,
+      stock: parseInt(event.target.stock.value),
+      sold: parseInt(0),
+      id: v4()
+    });
+  }
+}
+
+NewBeanForm.propTypes = {
+  onNewBeanCreation: PropTypes.func
+};
+
+export default NewBeanForm;
